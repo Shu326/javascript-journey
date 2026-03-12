9f8a8992205901a937296c44097d8fda22ab9fa1
SHUBHAM  RAJPUT <123220878+Shu326@users.noreply.github.com>
2026-03-12 13:58:18 +0530

    Add files via upload

---
+++ day1-practice.js
@@ -0,0 +1,37 @@
+//Problem 1 — Easy:
+
+const name='adity';
+let age = 22;
+let marks = 80;
+let ispass ='true';
+
+console.log(name);
+console.log(age);
+console.log(marks);
+console.log(ispass);
+
+
+//Problem 2 — Thoda sochna pada.(ek shope)
+const shopname='macbook';
+let itemprice='69,900';
+let discount=4000 ;
+let finalprice='65,900';
+ 
+itemprice=68900;
+discount='5000';
+finalprice='63900';
+
+console.log(shopname);
+console.log(itemprice);
+console.log(discount);
+console.log(finalprice);
+
+
+//Problem 3 — Tricky:
+const myName = "Shubham";
+myName = "Rahul";
+console.log(myName);
+
+// Yeh chalega ya error aayega?
+//const ki value kabhi nahi badlti — isliye error aaya
+// Kyun? — jawab bhi likho comment mein
---
+++ day1.js
@@ -0,0 +1,25 @@
+//Primitve data type
+let name="shubham";
+let age=22;
+let isstudent=true;
+let phone= null;
+let score; 
+
+console.log(name);
+console.log(age);
+console.log(isstudent);
+console.log(phone);
+console.log(score);
+
+// var let const
+
+const sitename="myapp";
+let userage = 22;
+let usercity ="jalna";
+
+userage = 23;
+usercity = "pune";
+
+console.log(sitename);
+console.log(userage);
+console.log(usercity);
