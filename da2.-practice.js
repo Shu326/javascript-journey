cad8084be705013d934dcb6e1e4391a807d34609
SHUBHAM  RAJPUT <123220878+Shu326@users.noreply.github.com>
2026-03-13 10:09:24 +0530

    Add files via upload

---
+++ da2.-practice.js
@@ -0,0 +1,10 @@
+// 1. Alert
+alert("Tera JS journey shuru ho gaya!");
+
+// 2. Confirm
+let result = confirm("Kya tu roz code karega?");
+console.log(result);
+
+// 3. Prompt
+let userName = prompt("Naam likho:");
+console.log("Hello " + userName + ", welcome!");
---
+++ day2.js
@@ -0,0 +1,10 @@
+// Alert
+alert("Namaste!");
+
+// Confirm
+let answer = confirm("JS pasand hai?");
+console.log(answer);
+
+// Prompt
+let userName = prompt("Apna naam likho:");
+console.log("Hello " + userName);
